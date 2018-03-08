789dad26a44a893ef453be854cc2389af762af40
Santhosh Sundar <36435848+santhoshsundar@users.noreply.github.com>
2018-03-08 20:54:04 +0530

    Update DataCollection.py

--- Source/DataCollection.py
+++ Source/DataCollection.py
@@ -2,10 +2,10 @@ import csv
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = 'bHLXKj7wSn2UH5yfLceg7wW53'
-consumer_secret = 'SXPcfKzfhVBU5WGxYwqyTVBgd0qourrd6IsUy97I4b6rC4d68t'
-access_token = '613251570-LeuCjbeYz7CcmwOW9F5MkH9gotumw7z2KuU5UTxI'
-access_secret = 'g50MUUtL2bNQfZMhQr5WWOybzPFdD9jfF5gAKucPXzmj4'
+consumer_key = ' '
+consumer_secret = ' '
+access_token = ' '
+access_secret = ' '
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
